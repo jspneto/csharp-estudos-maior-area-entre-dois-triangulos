98b204120d6edd6148e8a3abee9d6e0034096b39
José de Senna <josespnt1990@gmail.com>
2026-02-09 20:10:40 -0300

    refactor: cria objetos da classe Triangulo
    - Instancia dois objetos da classe Triangulo para representar os triângulos X e Y.
    - Substitui variáveis locais pelos atributos de classe correspondentes.

--- MaiorAreaTriangulos/Program.cs
+++ MaiorAreaTriangulos/Program.cs
@@ -7,21 +7,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        Triangulo x, y;
+
+        x = new Triangulo();
+        y = new Triangulo();
+
         Console.WriteLine("Entre com as medidas do triângulo X:");
-        double xA = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double xB = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double xC = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        x.A = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        x.B = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        x.C = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
         Console.WriteLine("Entre com as medidas do triângulo Y:");
-        double yA = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double yB = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double yC = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        y.A = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        y.B = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        y.C = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
-        double xP = (xA + xB + xC) / 2.0;
-        double yP = (yA + yB + yC) / 2.0;
+        double xP = (x.A + x.B + x.C) / 2.0;
+        double yP = (y.A + y.B + y.C) / 2.0;
 
-        double xArea = Math.Sqrt(xP * (xP - xA) * (xP - xB ) * (xP - xC));
-        double yArea = Math.Sqrt(yP * (yP - yA) * (yP - yB ) * (yP - yC));
+        double xArea = Math.Sqrt(xP * (xP - x.A) * (xP - x.B ) * (xP - x.C));
+        double yArea = Math.Sqrt(yP * (yP - y.A) * (yP - y.B ) * (yP - y.C));
 
         Console.WriteLine("Área de X: " + xArea.ToString("F4", CultureInfo.InvariantCulture));
         Console.WriteLine("Área de Y: " + yArea.ToString("F4", CultureInfo.InvariantCulture));
