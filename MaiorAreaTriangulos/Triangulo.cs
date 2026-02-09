d4935960daec50b65d388d2a3792a35c2bea829e
José de Senna <josespnt1990@gmail.com>
2026-02-09 20:32:59 -0300

    feat(area): cria metodo para calculo da area

--- MaiorAreaTriangulos/Triangulo.cs
+++ MaiorAreaTriangulos/Triangulo.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace MaiorAreaTriangulos;
 
 class Triangulo
@@ -5,4 +7,10 @@ class Triangulo
     public double A;
     public double B;
     public double C;
+
+    public double Area()
+    {
+        double p = (A + B + C) / 2.0;
+        return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+    }
 }
