bdad1dab46b1ac4241b9f27fe66e363d867fe1e2
José de Senna <josespnt1990@gmail.com>
2026-02-10 16:40:50 -0300

    refactor: sobrescre ToString para centralizar a exibicao da Area
    - Aplica override em ToString na classe Triangulo
    - Utiliza o metodo ToString para exibir as areas no programa principal

--- MaiorAreaTriangulos/Program.cs
+++ MaiorAreaTriangulos/Program.cs
@@ -10,32 +10,31 @@ class Program
         Triangulo x, y;
 
         x = new Triangulo();
+        x.Nome = "X";
         y = new Triangulo();
+        y.Nome = "Y";
 
-        Console.WriteLine("Entre com as medidas do triângulo X:");
+        Console.WriteLine("Entre com as medidas do triângulo " + x.Nome + ":");
         x.A = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
         x.B = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
         x.C = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
-        Console.WriteLine("Entre com as medidas do triângulo Y:");
+        Console.WriteLine("Entre com as medidas do triângulo " + y.Nome + ":");
         y.A = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
         y.B = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
         y.C = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
-        double xArea = x.Area();
-        double yArea = y.Area();
-
-        Console.WriteLine("Área de X: " + xArea.ToString("F4", CultureInfo.InvariantCulture));
-        Console.WriteLine("Área de Y: " + yArea.ToString("F4", CultureInfo.InvariantCulture));
+        Console.WriteLine(x.ToString());
+        Console.WriteLine(y.ToString());
         Console.Write("Maior área: ");
 
-        if (xArea >= yArea)
+        if (x.Area() >= y.Area())
         {
-            Console.WriteLine("X");
+            Console.WriteLine(x.Nome);
         }
         else
         {
-            Console.WriteLine("Y");
+            Console.WriteLine(y.Nome);
         }
     }
 }
